73684ff58a3f7de12658c8e2e5d7930a614f0a65
utaokihj <hj528345347@163.com>
2020-03-22 22:09:29 +0800

    tabber组件封装

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
-    
+    <router-view />
+    <main-tab-bar></main-tab-bar>
   </div>
 </template>
 
 <script>
+import MainTabBar from 'components/content/mainTabbar/MainTabBar'
+
 export default {
   name: 'app',
   components: {
-
+    MainTabBar
   }
 }
 </script>
 
-<style lang="less">
+<style>
 @import "assets/css/base.css"
 
 </style>
---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="tab-bar">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBar"
+};
+</script>
+
+<style lang="less" scoped>
+#tab-bar {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 49px;
+  background-color: #f6f6f6;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  box-shadow: 0 -3px 2px rgba(100, 100, 100, 0.1);
+}
+</style>
